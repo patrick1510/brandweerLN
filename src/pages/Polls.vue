21a9a4b1bc1941a5e573f5d85ffac946eb61989f
patrick15101996 <patrickshamoun@live.nl>
2021-06-15 11:23:58 +0200

    Made create poll collapsible

--- src/pages/Polls.vue
+++ src/pages/Polls.vue
@@ -12,101 +12,105 @@
       <header class="col-12 text-center text-weight-bold">
         <h1>{{ $route.meta.title }}</h1>
       </header>
-      <section
+      <q-expansion-item
         v-if="can('create', 'poll')"
-        class="row q-col-gutter-lg pollinputsection"
+        label="Maak een nieuwe poll aan"
+        header-class="text-white text-body1"
+        expand-icon-class="text-white"
       >
-        <div class="col-sm-12">
-          <q-card>
-            <q-card-section>
-              <h2>Invoer nieuwe poll</h2>
-            </q-card-section>
-            <q-card-section class="row items-center">
-              <!-- TITLE -->
-              <q-input
-                v-model="title"
-                class="col-xs-12 col-sm-6"
-                label="Titel van de poll"
-                outlined
-                hint=" "
-              />
-              <!-- DATETIME -->
-              <div class="q-pa-md col-xs-12 col-sm-6">
+        <section class="row q-col-gutter-lg pollinputsection">
+          <div class="col-sm-12">
+            <q-card>
+              <q-card-section class="row items-center">
+                <!-- TITLE -->
                 <q-input
-                  filled
-                  v-model="datetime"
-                  label="Datum & tijd van poll"
-                  hint="dd-mm-yyyy uu:mm"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="event" class="cursor-pointer">
-                      <q-popup-proxy
-                        transition-show="scale"
-                        transition-hide="scale"
-                      >
-                        <q-date v-model="datetime" mask="DD-MM-YYYY HH:mm">
-                          <div class="row items-center justify-end">
-                            <q-btn
-                              v-close-popup
-                              label="Close"
-                              color="primary"
-                              flat
-                            />
-                          </div>
-                        </q-date>
-                      </q-popup-proxy>
-                    </q-icon>
-                  </template>
-
-                  <template v-slot:append>
-                    <q-icon name="access_time" class="cursor-pointer">
-                      <q-popup-proxy
-                        transition-show="scale"
-                        transition-hide="scale"
-                      >
-                        <q-time
-                          v-model="datetime"
-                          mask="DD-MM-YYYY HH:mm"
-                          format24h
-                        >
-                          <div class="row items-center justify-end">
-                            <q-btn
-                              v-close-popup
-                              label="Close"
-                              color="primary"
-                              flat
-                            />
-                          </div>
-                        </q-time>
-                      </q-popup-proxy>
-                    </q-icon>
-                  </template>
-                </q-input>
-              </div>
-              <!-- DESCRIPTION -->
-              <div class="q-pr-md col-12">
-                <q-editor
-                  v-model="description"
-                  min-height="5rem"
-                  label="aanvullende informatie oefening (optioneel)"
+                  v-model="newPoll.title"
+                  class="col-xs-12 col-sm-6"
+                  label="Titel van de poll"
                   outlined
+                  hint=" "
                 />
-              </div>
-            </q-card-section>
-            <q-card-actions class="q-pa-md flex align-center justify-start">
-              <q-btn
-                :disabled="!validateNewPoll"
-                @click="createPoll()"
-                icon="add"
-                >maak aan</q-btn
-              >
-              <p v-if="!validateNewPoll" class="validate-new-poll-text">
-                Vul alle velden in
-              </p>
-            </q-card-actions>
-          </q-card>
-        </div>
-      </section>
+                <!-- DATETIME -->
+                <div class="q-pa-md col-xs-12 col-sm-6">
+                  <q-input
+                    filled
+                    v-model="newPoll.datetime"
+                    label="Datum & tijd van poll"
+                    hint="dd-mm-yyyy uu:mm"
+                  >
+                    <template v-slot:prepend>
+                      <q-icon name="event" class="cursor-pointer">
+                        <q-popup-proxy
+                          transition-show="scale"
+                          transition-hide="scale"
+                        >
+                          <q-date
+                            v-model="newPoll.datetime"
+                            mask="DD-MM-YYYY HH:mm"
+                          >
+                            <div class="row items-center justify-end">
+                              <q-btn
+                                v-close-popup
+                                label="Close"
+                                color="primary"
+                                flat
+                              />
+                            </div>
+                          </q-date>
+                        </q-popup-proxy>
+                      </q-icon>
+                    </template>
+
+                    <template v-slot:append>
+                      <q-icon name="access_time" class="cursor-pointer">
+                        <q-popup-proxy
+                          transition-show="scale"
+                          transition-hide="scale"
+                        >
+                          <q-time
+                            v-model="newPoll.datetime"
+                            mask="DD-MM-YYYY HH:mm"
+                            format24h
+                          >
+                            <div class="row items-center justify-end">
+                              <q-btn
+                                v-close-popup
+                                label="Close"
+                                color="primary"
+                                flat
+                              />
+                            </div>
+                          </q-time>
+                        </q-popup-proxy>
+                      </q-icon>
+                    </template>
+                  </q-input>
+                </div>
+                <!-- DESCRIPTION -->
+                <div class="q-pr-md col-12">
+                  <q-editor
+                    v-model="newPoll.description"
+                    min-height="5rem"
+                    label="aanvullende informatie oefening (optioneel)"
+                    outlined
+                  />
+                </div>
+              </q-card-section>
+              <q-card-actions class="q-pa-md flex align-center justify-start">
+                <q-btn
+                  :disabled="!validateNewPoll"
+                  @click="createPoll()"
+                  icon="add"
+                  >Maak aan</q-btn
+                >
+                <p v-if="!validateNewPoll" class="validate-new-poll-text">
+                  Vul alle velden in
+                </p>
+              </q-card-actions>
+            </q-card>
+          </div>
+        </section>
+      </q-expansion-item>
       <section class="row q-col-gutter-lg pollssection">
         <h1 v-if="Object.keys(polls).length === 0">Polls aan het laden...</h1>
         <div
@@ -132,6 +136,7 @@
 
 <script>
 import { ConvertDateTime } from "../helpers.js";
+import { Notify } from "quasar";
 import PollCard from "../components/PollCard.vue";
 export default {
   name: "Polls",
@@ -141,9 +146,11 @@ export default {
   data() {
     return {
       isLoading: false,
-      title: "",
-      datetime: "",
-      description: "",
+      newPoll: {
+        title: "",
+        datetime: "",
+        description: ""
+      },
       refreshedPolls: {}
     };
   },
@@ -169,7 +176,11 @@ export default {
       return this.$store.getters["auth/userID"];
     },
     validateNewPoll() {
-      return this.title && this.datetime && this.description;
+      return (
+        this.newPoll.title &&
+        this.newPoll.datetime &&
+        !(!this.newPoll.description || this.newPoll.description === "<br>")
+      );
     }
   },
   methods: {
@@ -186,9 +197,7 @@ export default {
       this.isLoading = true;
       this.$store
         .dispatch("polls/getPolls")
-        .then(response => {
-          console.log("received polls");
-        })
+        .then(polls => {})
         .catch(err => {
           console.log(err);
         })
@@ -196,14 +205,14 @@ export default {
           this.isLoading = false;
         });
     },
-    createPoll: function() {
+    createPoll() {
       this.isLoading = true;
-      if (this.datetime) {
-        const CnvrtDT = new ConvertDateTime(this.datetime);
+      if (this.newPoll.datetime) {
+        const CnvrtDT = new ConvertDateTime(this.newPoll.datetime);
         this.$store
           .dispatch("polls/createPoll", {
-            title: this.title,
-            description: this.description,
+            title: this.newPoll.title,
+            description: this.newPoll.description,
             datetimeISO: CnvrtDT.NLtoISO()
           })
           .then(poll => {
@@ -212,11 +221,14 @@ export default {
               color: "green",
               textColor: "white"
             });
+            this.newPoll.title = "";
+            this.newPoll.datetime = "";
+            this.newPoll.description = "";
           })
           .catch(err => {
             Notify.create({
-              title: "Het aanmaken van de poll ging mis",
-              message: "Zijn alle velden goed ingevuld?",
+              message: "Het aanmaken van de poll ging mis",
+              caption: "Zijn alle velden goed ingevuld?",
               icon: "error",
               color: "primary",
               textColor: "white"
