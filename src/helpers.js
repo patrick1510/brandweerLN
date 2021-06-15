f0fa78eaf537f82139e1e98d7d653f5b3ff1e610
patrick15101996 <patrickshamoun@live.nl>
2021-06-15 10:55:02 +0200

    Added datetime converter in helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,17 @@
-export function convertDate(date) {
-  // converts this format: 2021-06-30T16:23:00
-  return date.replace("T", " ").slice(0, -3);
-}
+export class ConvertDateTime {
+  constructor (datetime) {
+    this.datetime = datetime;
+  }
+  NLtoISO() {
+    const datetimeArr = this.datetime.split(' ');
+    const date = datetimeArr[0].split('-').reverse().join('-');
+    const time = datetimeArr[1];
+    return date + 'T' + time;
+  }
+  ISOtoNL() {
+    const datetimeArr = this.datetime.split('T');
+    const date = datetimeArr[0].split('-').reverse().join('-');
+    const time = datetimeArr[1].slice(0, -3);
+    return date + ' ' + time;
+  }
+};
